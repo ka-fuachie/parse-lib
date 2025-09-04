b9747ea990006a1d3da2d3dada8215fa3130e5da
Kwabena Fuachie <kafuachie11@gmail.com>
2025-09-04 22:20:43 +0000

    :sparkles: add base functionality and literal parser

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 - <https://blog.bruce-hill.com/packrat-parsing-from-scratch>
 - <https://medium.com/@gvanrossum_83706/peg-parsing-series-de5d41b2ed60>
 - <https://github.com/francisrstokes/arcsecond>
-- <https://www.youtube.com/watch?v=6oQLRhw5Ah0>
+- <https://www.youtube.com/playlist?list=PLP29wDx6QmW5yfO1LAgO8kU3aQEj8SIrU>
--- index.js
+++ index.js
@@ -1,1 +1,142 @@
+const ParserStateStatus = /**@type{const}*/({
+  COMPLETE: "complete",
+  PARTIAL: "partial",
+  ERROR: "error",
+})
 
+/** @typedef {typeof ParserStateStatus[keyof typeof ParserStateStatus]} ParserStateStatusType */
+
+/**
+  * @typedef {Object} SuccessParserState
+  * @property {Object} input
+  * @property {string} input.value
+  * @property {boolean} input.done
+  * @property {number} index
+  * @property {Exclude<ParserStateStatusType, "error">} status
+  * @property {any} result
+  * @property {null} error
+  * @property {Map<Parser, Map<number, ParserResult>>} cacheMap
+  */
+
+/**
+  * @typedef {Object} ErrorParserState
+  * @property {Object} input
+  * @property {string} input.value
+  * @property {boolean} input.done
+  * @property {number} index
+  * @property {Extract<ParserStateStatusType, "error">} status
+  * @property {null} result
+  * @property {Error} error
+  * @property {Map<Parser, Map<number, ParserResult>>} cacheMap
+  */
+
+/**
+  * @typedef {SuccessParserState | ErrorParserState} ParserState
+  */
+
+/**
+  * @typedef {Pick<ParserState, "index" | "status" | "error" | "result">} ParserResult
+  */
+
+/**
+  * @callback StateTransformFn
+  * @param {SuccessParserState} parserState
+  * @returns {ParserState}
+  */
+
+/**
+  * @param {ParserState} state
+  * @returns {state is ErrorParserState}
+  */
+function isErrorState(state) {
+  return state.status === ParserStateStatus.ERROR
+}
+
+class Parser {
+  /** @type {StateTransformFn} */
+  #stateTransformFn
+
+  /**
+    * @param {StateTransformFn} stateTransformFn
+    */
+  constructor(stateTransformFn) {
+    this.#stateTransformFn = stateTransformFn
+  }
+
+  /** @param {ParserState} parserState */
+  transform(parserState) {
+    if(isErrorState(parserState)) return parserState
+
+    let cache = parserState.cacheMap.get(this)
+    if(cache == null) {
+      cache = new Map()
+      parserState.cacheMap.set(this, cache)
+    }
+    const cachedResult = cache.get(parserState.index)
+    if(cachedResult != null) {
+      return {
+        ...parserState,
+        ...cachedResult,
+      }
+    }
+
+    const nextState = this.#stateTransformFn(parserState)
+    cache.set(parserState.index, {
+      index: nextState.index,
+      status: nextState.status,
+      error: nextState.error,
+      result: nextState.result,
+    })
+    return nextState
+  }
+
+  /** @param {string} input */
+  parseString(input) {
+    /** @satisfies {ParserState} */
+    const parserState = {
+      input: { value: input, done: true },
+      index: 0,
+      status: ParserStateStatus.PARTIAL,
+      result: null,
+      error: null,
+      cacheMap: new Map(),
+    }
+
+    return this.transform(parserState)
+  }
+}
+
+/** @param {string} s */
+function literal(s) {
+  return new Parser(parserState => {
+    const remainingInput = parserState.input.value.slice(parserState.index)
+    if(!parserState.input.done && remainingInput.length < s.length && s.startsWith(remainingInput)) {
+      return {
+        ...parserState,
+        index: parserState.index + remainingInput.length,
+        status: ParserStateStatus.PARTIAL,
+        result: s,
+      }
+    }
+
+    if(!parserState.input.value.startsWith(s, parserState.index)) {
+      return {
+        ...parserState,
+        status: ParserStateStatus.ERROR,
+        error: new Error(`Expected "${s}", but got "${parserState.input.value.slice(parserState.index, parserState.index + s.length)}"`),
+      }
+    }
+
+    return {
+      ...parserState,
+      index: parserState.index + s.length,
+      status: ParserStateStatus.COMPLETE,
+      result: s,
+    }
+  })
+}
+
+const parser = literal("Hello")
+console.log(parser.parseString("Hell"))
+console.log(parser.parseString("Hello, world!"))
+console.log(parser.parseString("Hi, world!"))
